8b84ca64f5203a8b167adaa7cc33f86ec963f118
qfchenMSFT <chenfa@microsoft.com>
2024-10-25 08:22:19 -0500

    deployonly

--- .github/workflows/index.js
+++
@@ -1,9 +0,0 @@
-// .github/actions/checkout/index.js
-const { execSync } = require('child_process');
-
-try {
-  execSync('git checkout main', { stdio: 'inherit' });
-} catch (error) {
-  console.error(`Error checking out the repository: ${error.message}`);
-  process.exit(1);
-}
